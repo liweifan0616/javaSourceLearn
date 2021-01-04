9c8ee291250e74bc7acbef0eee73998745bb8358
liweifan <lwf474686@ly.com>
2021-01-04 15:14:05 +0800

    first commit

---
+++ src/com/lwf/test/hashmap/Test.java
@@ -0,0 +1,18 @@
+package com.lwf.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author liweifan
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map<Integer,String> hashMap = new HashMap<>();
+        hashMap.put(1,"小明");
+        hashMap.put(2,"小红");
+        hashMap.put(3,"小强");
+        System.out.println("test");
+    }
+}
